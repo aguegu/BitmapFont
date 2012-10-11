430e1356d3ce3e68cbe039eb908ed96ec64ba0db
weihong.guan <weihong.guan@gmail.com>
2012-10-12 02:59:41 +0800

    complete sh

--- BitmapFont.sh
+++ BitmapFont.sh
@@ -1,7 +1,25 @@
 Debug/BitmapFont font/ASC12 output/ASC12.txt 12 1 1 0x20
+
 Debug/BitmapFont font/ASC16 output/ASC16.txt 16 1 1 0x00
+
 Debug/BitmapFont font/ASC48 output/ASC48.txt 144 3 1 0x20
+
 Debug/BitmapFont font/HZK12 output/HZK12.txt 24 2 2 0xA1
+
 Debug/BitmapFont font/HZK16 output/HZK16.txt 32 2 2 0xA1
 Debug/BitmapFont font/HZK16F output/HZK16F.txt 32 2 2 0xA1
 Debug/BitmapFont font/HZK16S output/HZK16S.txt 32 2 2 0xA1
+
+Debug/BitmapFont font/HZK24F output/HZK24F.txt 72 3 2 0xB0
+Debug/BitmapFont font/HZK24H output/HZK24H.txt 72 3 2 0xB0
+Debug/BitmapFont font/HZK24S output/HZK24S.txt 72 3 2 0xB0
+Debug/BitmapFont font/HZK24T output/HZK24T.txt 72 3 2 0xA1
+
+Debug/BitmapFont font/HZK32 output/HZK32.txt 128 4 2 0xA1
+
+Debug/BitmapFont font/HZK40S output/HZK40S.txt 200 5 2 0xB0
+Debug/BitmapFont font/HZK40T output/HZK40T.txt 200 5 2 0xA1
+
+Debug/BitmapFont font/HZK48S output/HZK48S.txt 288 6 2 0xB0
+Debug/BitmapFont font/HZK48T output/HZK48T.txt 288 6 2 0xA1
+Debug/BitmapFont font/HZK48 output/HZK48.txt 288 6 2 0xA1
