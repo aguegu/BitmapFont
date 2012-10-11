0cd7ae9eaddc664be7fc4752a8e1a6494c248412
weihong.guan <weihong.guan@gmail.com>
2012-10-12 00:47:06 +0800

    add header in output

--- src/BitmapFont.cpp
+++ src/BitmapFont.cpp
@@ -13,6 +13,50 @@
 
 using namespace std;
 
+void printString(ofstream & fout, unsigned char *p, int length)
+{
+	char tmp[5];
+
+	for (int i = 0; i < length; i++)
+	{
+		sprintf(tmp, "0x%02x", p[i]);
+		fout << tmp;
+		fout << ", ";
+
+		if (i % 8 == 7)
+			fout << endl;
+	}
+	fout << endl;
+}
+
+void printPattern(ofstream & fout, unsigned char *p, int length,
+		int byte_in_row)
+{
+	char c_on[] =
+	{ 0xe2, 0x97, 0xbc };
+	char c_off[] =
+	{ 0xe2, 0x97, 0xbb };
+
+	for (int i = 0; i < length;)
+	{
+		fout << "//  ";
+
+		for (int k = byte_in_row; k > 0; k--)
+		{
+			unsigned char temp = p[i];
+			for (int j = 0; j < 8; j++)
+			{
+				fout.write(temp >= 0x80 ? c_on : c_off, 3);
+				temp <<= 1;
+			}
+			i++;
+		}
+		fout << endl;
+	}
+
+	fout << endl;
+}
+
 int main(int argc, char* argv[])
 {
 	int length = atoi(argv[3]);
@@ -34,24 +78,22 @@ int main(int argc, char* argv[])
 	ifstream fin(argv[1], ios::binary);
 	ofstream fout(argv[2]);
 
-	char tmp[5];
-	char c_on[] =
-	{ 0xe2, 0x97, 0xbc };
-	char c_off[] =
-	{ 0xe2, 0x97, 0xbb };
+	fout << hex;
+
 	while (fin.read((char *) p, length))
 	{
-		//fout.write(p, length);
-		for (int i = 0; i < length; i++)
-		{
-			sprintf(tmp, "0x%02x", p[i]);
-			fout << tmp;
-			fout << ", ";
-		}
+		long pos = fin.tellg();
+        long index = pos / length - 1;
 
-		fout.write(c_on, 3);
-		fout.write(c_off, 3);
+		fout << "// 0x" << fin.tellg();
+		fout << ", 0x" << index;
+		fout << ", 0x" << index + code_offset;
+		fout << ", \" " << (char)(index + code_offset) << " \"";
 		fout << endl;
+
+		printString(fout, p, length);
+		fout << endl;
+		printPattern(fout, p, length, byte_in_row);
 
 		fout.flush();
 	}
