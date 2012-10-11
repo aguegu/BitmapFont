e66811f29a3301f0ef6b1079caee1aa7faa12a20
weihong.guan <weihong.guan@gmail.com>
2012-10-12 02:30:35 +0800

    improve output

--- BitmapFont.sh
+++ BitmapFont.sh
@@ -1,1 +1,7 @@
 Debug/BitmapFont font/ASC12 output/ASC12.txt 12 1 1 0x20
+Debug/BitmapFont font/ASC16 output/ASC16.txt 16 1 1 0x00
+Debug/BitmapFont font/ASC48 output/ASC48.txt 144 3 1 0x20
+Debug/BitmapFont font/HZK12 output/HZK12.txt 24 2 2 0xA1
+Debug/BitmapFont font/HZK16 output/HZK16.txt 32 2 2 0xA1
+Debug/BitmapFont font/HZK16F output/HZK16F.txt 32 2 2 0xA1
+Debug/BitmapFont font/HZK16S output/HZK16S.txt 32 2 2 0xA1
--- src/BitmapFont.cpp
+++ src/BitmapFont.cpp
@@ -10,17 +10,56 @@
 #include <iconv.h>
 #include <fstream>
 #include <cstdlib>
+#include <cstring>
 
 using namespace std;
 
+const char c_on[] =
+{ 0xe2, 0x96, 0xa0 };
+const char c_off[] =
+{ 0xe2, 0x96, 0xa1 };
+
+int convertCode(char *inbuf, unsigned long inlen, char *outbuf,
+		unsigned long outlen)
+{
+	iconv_t cd;
+	char **pin = &inbuf;
+	char **pout = &outbuf;
+
+	cd = iconv_open("UTF-8", "GB2312");
+	if (cd == 0)
+		return -1;
+	memset(outbuf, 0, outlen);
+	if (iconv(cd, pin, &inlen, pout, &outlen) == (size_t) (-1))
+		return -1;
+	iconv_close(cd);
+	return 0;
+}
+
+char *byteString(unsigned char c)
+{
+	char *tmp = new char[5];
+	sprintf(tmp, "0x%02x", c);
+	return tmp;
+}
+
+char *byteStringPure(unsigned char c)
+{
+	char *tmp = new char[3];
+	sprintf(tmp, "%02x", c);
+	return tmp;
+}
+
+
 void printString(ofstream & fout, unsigned char *p, int length)
 {
-	char tmp[5];
+//	char tmp[5];
 
 	for (int i = 0; i < length; i++)
 	{
-		sprintf(tmp, "0x%02x", p[i]);
-		fout << tmp;
+//		sprintf(tmp, "0x%02x", p[i]);
+//		fout << tmp;
+		fout << byteString(p[i]);
 		fout << ", ";
 
 		if (i % 8 == 7)
@@ -32,10 +71,6 @@ void printString(ofstream & fout, unsigned char *p, int length)
 void printPattern(ofstream & fout, unsigned char *p, int length,
 		int byte_in_row)
 {
-	char c_on[] =
-	{ 0xe2, 0x97, 0xbc };
-	char c_off[] =
-	{ 0xe2, 0x97, 0xbb };
 
 	for (int i = 0; i < length;)
 	{
@@ -48,6 +83,7 @@ void printPattern(ofstream & fout, unsigned char *p, int length,
 			{
 				fout.write(temp >= 0x80 ? c_on : c_off, 3);
 				temp <<= 1;
+				fout << ' ';
 			}
 			i++;
 		}
@@ -63,16 +99,18 @@ int main(int argc, char* argv[])
 	int byte_in_row = atoi(argv[4]);
 	int byte_in_code = atoi(argv[5]);
 	int code_offset;
-	sscanf(argv[6], "0x%02x", &code_offset);
+	sscanf(argv[6], "0x%02X", &code_offset);
 
 	//cout << "BitmapFont" << endl; // prints !!!Hello World!!!
-	cout << argv[1] << endl;
-	cout << argv[2] << endl;
-	cout << length << endl;
-	cout << byte_in_row << endl;
-	cout << byte_in_code << endl;
-	cout << code_offset << endl;
+	cout << argv[1] << ", ";
+	cout << argv[2] << ", ";
+	cout << length << ", ";
+	cout << byteString(byte_in_row) << ", ";
+	cout << byteString(byte_in_code) << ", ";
+	cout << byteString(code_offset) << endl;
 
+	char s_in[8];
+	char s_out[8];
 	unsigned char *p = new unsigned char[length];
 
 	ifstream fin(argv[1], ios::binary);
@@ -83,16 +121,32 @@ int main(int argc, char* argv[])
 	while (fin.read((char *) p, length))
 	{
 		long pos = fin.tellg();
-        long index = pos / length - 1;
+		long index = pos / length - 1;
 
 		fout << "// 0x" << fin.tellg();
-		fout << ", 0x" << index;
-		fout << ", 0x" << index + code_offset;
-		fout << ", \" " << (char)(index + code_offset) << " \"";
+		fout << ", 0x" << index << ", ";
+
+		memset(s_in, 0x00, 8);
+		if (byte_in_code == 1)
+		{
+			s_in[0] = index + code_offset;
+			fout << "(GB2312)" << byteString(s_in[0]) << ", ";
+		}
+		else
+		{
+			s_in[0] = code_offset + (index / 94);
+			s_in[1] = 0xA1 + (index % 94);
+			fout << "(GB2312)" << byteString(s_in[0]);
+			fout << byteStringPure(s_in[1]) << ", ";
+		}
+
+		convertCode(s_in, 8, s_out, 8);
+		fout << "(UTF-8)" << byteString(s_out[0]) << byteStringPure(s_out[1]) << byteStringPure(s_out[2]);
+		fout << ", \" " << s_out << " \"";
+
 		fout << endl;
 
 		printString(fout, p, length);
-		fout << endl;
 		printPattern(fout, p, length, byte_in_row);
 
 		fout.flush();
