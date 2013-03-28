fd7dd723d26e59a815a0871e63fa077825f11c00
Weihong.Guan <weihong.Guan@gmail.com>
2013-03-28 13:42:00 +0800

    add test for bytestring

--- test/block_test.cpp
+++ test/block_test.cpp
@@ -16,5 +16,21 @@ TEST(Block, testSlipByte)
 	EXPECT_EQ(0x01, Block::flipByte(0x80));
 	EXPECT_EQ(0x0f, Block::flipByte(0xf0));
 	EXPECT_EQ(0xc0, Block::flipByte(0x03));
-	
+	EXPECT_EQ(0x12, Block::flipByte(0x48));
+}
+
+TEST(Block, testByteStringPure)
+{
+	EXPECT_STREQ("00", Block::byteStringPure(0x00).c_str());
+	EXPECT_STREQ("ff", Block::byteStringPure(0xff).c_str());
+	EXPECT_STREQ("e7", Block::byteStringPure(0xe7).c_str());
+	EXPECT_STREQ("93", Block::byteStringPure(0x93).c_str());
+}
+
+TEST(Block, testByteString)
+{
+	EXPECT_STREQ("0x00", Block::byteString(0x00).c_str());
+	EXPECT_STREQ("0xff", Block::byteString(0xff).c_str());
+	EXPECT_STREQ("0xe7", Block::byteString(0xe7).c_str());
+	EXPECT_STREQ("0x93", Block::byteString(0x93).c_str());
 }
